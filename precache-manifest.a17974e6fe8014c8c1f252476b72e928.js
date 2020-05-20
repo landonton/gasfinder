8c02795bdf6f450e27e37949872b66b7e1924e56
landon <landon@Landon.local>
2020-05-19 17:59:43 -0700

    Updates

---
+++ precache-manifest.a17974e6fe8014c8c1f252476b72e928.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1ac5b14d3dc8df528716aa6a274605",
+    "url": "/gasfinder/index.html"
+  },
+  {
+    "revision": "46a4e89356a5062b23cd",
+    "url": "/gasfinder/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d950689e3822e9ae90db",
+    "url": "/gasfinder/static/css/main.322030d2.chunk.css"
+  },
+  {
+    "revision": "46a4e89356a5062b23cd",
+    "url": "/gasfinder/static/js/2.ad155eb7.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/gasfinder/static/js/2.ad155eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d950689e3822e9ae90db",
+    "url": "/gasfinder/static/js/main.44d9c26e.chunk.js"
+  },
+  {
+    "revision": "ea3cd18c60265b82e3da",
+    "url": "/gasfinder/static/js/runtime-main.162fd83d.js"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/gasfinder/static/media/Pacifico-Regular.9b94499c.ttf"
+  }
+]);
